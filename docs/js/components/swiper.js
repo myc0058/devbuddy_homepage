211b1a642898648e3c0d6337c44b3b60c29de690
Young Chul Mo <myc0058@gmail.com>
2025-12-17 10:14:37 +0900

    formatting, add tally, complete index.html

--- docs/js/components/swiper.js
+++ docs/js/components/swiper.js
@@ -31,7 +31,7 @@ $(window).load(function(){
     // Swiper Clients
     var swiper = new Swiper('.swiper-testimonials', {
         speed: 1000,
-        autoplay: 10000,
+        autoplay: 5000,
         slidesPerView: 1,
         loop: true,
     });
